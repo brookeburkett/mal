ec3ad3e0c46ebfa8d3eba4e34d430ef17533da3a
Brooke <blake.a.burkett@gmail.com>
2022-02-14 13:23:22 -0500

    add console history, update to new c++ standard.

--- impls/cpp/step0_repl.cpp
+++ impls/cpp/step0_repl.cpp
@@ -1,21 +1,29 @@
 #include <string>
 #include <iostream>
 
+#include "linenoise.hpp"
+
 std::string READ(const std::string& input);
 std::string EVAL(const std::string& ast);
 std::string PRINT(const std::string& ast);
 std::string rep(const std::string& input);
 
+
 int main(int argc, char* argv[])
 {
+  const auto history_path = "~/mal_history.txt";
+  linenoise::LoadHistory(history_path);
   std::string input;
   for (;;)
   {
-    std::cout << "user> ";
-    if (!std::getline(std::cin, input))
+    auto quit = linenoise::Readline("user> ", input);
+    if (quit)
       break;
     std::cout << rep(input) << std::endl; 
+    linenoise::AddHistory(input.c_str());
   }
+
+  linenoise::SaveHistory(history_path);
 }
 
 std::string READ(const std::string& input)
